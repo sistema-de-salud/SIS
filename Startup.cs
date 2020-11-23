7bfbedbfa0a5fa6c9bc58d1a11138262f67d1219
juancarlosenestrilla <72900814+juancarlosenestrilla@users.noreply.github.com>
2020-11-22 22:12:21 -0600

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaClinico.Startup))]
+namespace SistemaClinico
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
